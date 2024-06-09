ee3ee21c9d5d47f8b1ade03a385b316dfaa97375
Schefflera-Arboricola <1509aditi@gmail.com>
2024-06-09 20:28:26 +0530

    updated interface.py and number_of_isolates

--- nx_j4f/algorithms/isolate.py
+++ nx_j4f/algorithms/isolate.py
@@ -1,10 +1,8 @@
-import networkx as nx
-
 __all__ = ["number_of_isolates"]
 
 
-def number_of_isolates(G):
+def number_of_isolates(G, name=""):
     if hasattr(G, "graph_object"):
         G = G.graph_object
 
-    return "Hi"
+    return "Hi" + name
